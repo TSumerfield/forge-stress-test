3103f9f805dc50efe54ec4b2b24db4a9c416d709
TSumerfield <103722856+TSumerfield@users.noreply.github.com>
2026-08-16 16:59:59 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,17 @@
-    
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&display=swap" 
+          rel="stylesheet" 
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
